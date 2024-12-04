bbeb9797cebc5c6d6cf0cc7dac7f4b02a6d78d3d
rjmodev <psreginaldo@hotmail.com>
2024-12-04 00:03:51 -0300

    exercise1

---
+++ parimpar1.c
@@ -0,0 +1,17 @@
+//Verificar se um Número é Par ou Ímpar
+//Escreva um programa que solicite ao usuário um número e verifique se ele é par ou ímpar.
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Verificando se o digitado é par ou ímpar!!");
+    printf("Escreva um número inteiro para verificar: ");
+    scanf ("%d", &number);
+    if (number % 2== 0){
+        printf("Verifiquei aqui e consta que o %d é par!!", number);
+    } else{
+        printf("Verifiquei aqui e consta que o %d é ímpar!!", number);    
+    }
+    return(0);
+}
